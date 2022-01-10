320d831f4b5f9121dbb5340053d6e9bd05090433
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-01-11 00:17:12 +0600

    create task model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,6 +15,19 @@ app.post("/users", (req, res) => {
     .save()
     .then(() => {
       res.send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
     })
     .catch((e) => {
       res.status(400).send(e);
