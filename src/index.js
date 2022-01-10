3a0e5ee95b8793d4bdacb10e84acd3862d4b6bf1
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-01-11 00:24:01 +0600

    get task api

--- src/index.js
+++ src/index.js
@@ -47,6 +47,45 @@ app.get("/users/:id", (req, res) => {
     });
 });
 
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
