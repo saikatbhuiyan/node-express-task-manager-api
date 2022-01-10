feb6397f273eb413ec281a7eb1b93d545de48f14
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-01-10 23:57:28 +0600

    user model create

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.post("/users", (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
