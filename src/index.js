9d3c3a3af08e2eafc8f373aebf230b43f6d96647
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-01-22 01:47:36 +0600

    update task router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
