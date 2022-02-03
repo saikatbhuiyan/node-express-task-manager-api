d7f66807ef912923c0eeb6ff92c0989a6c00ae09
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-03 16:37:28 +0600

    authention user end point

--- src/models/user.js
+++ src/models/user.js
@@ -102,3 +102,4 @@ userSchema.pre("save", async function (next) {
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
+
--- src/routers/user.js
+++ src/routers/user.js
@@ -55,23 +55,7 @@ router.get("/users/me", auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-router.patch("/users/:id", async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["name", "email", "password", "age"];
   const isValidOperation = updates.every((update) =>
@@ -83,33 +67,83 @@ router.patch("/users/:id", async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id);
-
-    updates.forEach((update) => (user[update] = req.body[update]));
-    await user.save();
-
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
+    // const user = await User.findByIdAndDelete(req.user._id);
 
-    if (!user) {
-      return res.status(404).send();
-    }
-
-    res.send(user);
+    // if (!user) {
+    //   return res.status(404).send();
+    // }
+    await req.user.remove();
+    res.send(req.user);
   } catch (e) {
     res.status(500).send();
   }
 });
+
+// router.get("/users/:id", async (req, res) => {
+//   const _id = req.params.id;
+
+//   try {
+//     const user = await User.findById(_id);
+
+//     if (!user) {
+//       return res.status(404).send();
+//     }
+
+//     res.send(user);
+//   } catch (e) {
+//     res.status(500).send();
+//   }
+// });
+
+// router.patch("/users/:id", async (req, res) => {
+//   const updates = Object.keys(req.body);
+//   const allowedUpdates = ["name", "email", "password", "age"];
+//   const isValidOperation = updates.every((update) =>
+//     allowedUpdates.includes(update)
+//   );
+
+//   if (!isValidOperation) {
+//     return res.status(400).send({ error: "Invalid updates!" });
+//   }
+
+//   try {
+//     const user = await User.findById(req.params.id);
+
+//     updates.forEach((update) => (user[update] = req.body[update]));
+//     await user.save();
+
+//     if (!user) {
+//       return res.status(404).send();
+//     }
+
+//     res.send(user);
+//   } catch (e) {
+//     res.status(400).send(e);
+//   }
+// });
+
+// router.delete("/users/:id", async (req, res) => {
+//   try {
+//     const user = await User.findByIdAndDelete(req.params.id);
+
+//     if (!user) {
+//       return res.status(404).send();
+//     }
+
+//     res.send(user);
+//   } catch (e) {
+//     res.status(500).send();
+//   }
+// });
 
 module.exports = router;
