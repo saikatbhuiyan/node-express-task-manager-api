00a885f551fe722abf897f01c69434f0d3e402a0
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-03 15:40:10 +0600

    hide password and tokens from object

--- src/models/user.js
+++ src/models/user.js
@@ -51,6 +51,17 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+// it will run when we call user object
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+};
+
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "thisismynewcourse");
