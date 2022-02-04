eb300a86d570241d7030ddec15df3a3d89b58d87
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-04 17:50:07 +0600

    sort task by ase and desc order

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,11 +19,18 @@ router.post("/tasks", auth, async (req, res) => {
 
 // GET /tasks?completed=true
 // GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) {
     match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
 
   try {
@@ -38,6 +45,7 @@ router.get("/tasks", auth, async (req, res) => {
         options: {
           limit: parseInt(req.query.limit),
           skip: parseInt(req.query.skip),
+          sort,
         },
       })
       .execPopulate();
