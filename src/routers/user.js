b0236e3d7dc540b58246054f5cbe66e3c1543b57
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-08 03:32:56 +0600

    send welcome and cancelation message

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,8 @@ const multer = require("multer");
 const sharp = require("sharp");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
+const { sendWelcomeEmail, sendCancelationEmail } = require("../emails/account");
+
 const router = new express.Router();
 
 router.post("/users", async (req, res) => {
@@ -10,6 +12,7 @@ router.post("/users", async (req, res) => {
 
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);
     const token = await user.generateAuthToken();
     res.status(201).send({ user, token });
   } catch (e) {
@@ -85,6 +88,7 @@ router.delete("/users/me", auth, async (req, res) => {
     //   return res.status(404).send();
     // }
     await req.user.remove();
+    sendCancelationEmail(req.user.email, req.user.name);
     res.send(req.user);
   } catch (e) {
     res.status(500).send();
