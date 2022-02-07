b1790338beafa1ab6e8845c32550f0e8ec74564f
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-08 04:50:35 +0600

    use env-cmd for env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://sami:<password>@saikat.2ccr0.mongodb.net/task?retryWrites=true&w=majority";
-
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   // useCreateIndex: true,
+  useFindAndModify: true,
 });
--- src/emails/account.js
+++ src/emails/account.js
@@ -1,13 +1,11 @@
 const sgMail = require("@sendgrid/mail");
 
-const sendgridAPIKey = "";
-
-sgMail.setApiKey(sendgridAPIKey);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendWelcomeEmail = (email, name) => {
   sgMail.send({
     to: email,
-    from: "",
+    from: `${process.env.FROM_EMAIL}`,
     subject: "Thanks for joining in!",
     text: `Welcome to the app, ${name}. Let me know how you get along with the app.`,
   });
