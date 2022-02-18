98055238d0d175579ada08983e1346e5ecb93562
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-19 02:13:15 +0600

    async fun test using jest

---
+++ src/mathTest.js
@@ -0,0 +1,28 @@
+const calculateTip = (total, tipPercent = 0.25) => total + total * tipPercent;
+
+const fahrenheitToCelsius = (temp) => {
+  return (temp - 32) / 1.8;
+};
+
+const celsiusToFahrenheit = (temp) => {
+  return temp * 1.8 + 32;
+};
+
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (a < 0 || b < 0) {
+        return reject("Numbers must be non-negative");
+      }
+
+      resolve(a + b);
+    }, 2000);
+  });
+};
+
+module.exports = {
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
+  add,
+};
---
+++ tests/math.test.js
@@ -0,0 +1,56 @@
+const {
+  calculateTip,
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+  add,
+} = require("../src/math");
+
+test("Should calculate total with tip", () => {
+  const total = calculateTip(10, 0.3);
+  expect(total).toBe(13);
+});
+
+test("Should calculate total with default tip", () => {
+  const total = calculateTip(10);
+  expect(total).toBe(12.5);
+});
+
+test("Should convert 32 F to 0 C", () => {
+  const temp = fahrenheitToCelsius(32);
+  expect(temp).toBe(0);
+});
+
+test("Should convert 0 C to 32 F", () => {
+  const temp = celsiusToFahrenheit(0);
+  expect(temp).toBe(32);
+});
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000)
+// })
+
+test("Should add two numbers", (done) => {
+  add(2, 3).then((sum) => {
+    expect(sum).toBe(5);
+    done();
+  });
+});
+
+test("Should add two numbers async/await", async () => {
+  const sum = await add(10, 22);
+  expect(sum).toBe(32);
+});
+
+//
+// Why test?
+//
+// - Saves time
+// - Creates reliable software
+// - Gives flexibility to developers
+//   - Refactoring
+//   - Collaborating
+//   - Profiling
+// - Peace of mind
