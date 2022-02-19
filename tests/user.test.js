e3dd602801269a49cd730be2dd3c8138120ad950
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-19 07:33:33 +0600

    basic test for some  user api

---
+++ tests/user.test.js
@@ -0,0 +1,45 @@
+const request = require("supertest");
+const app = require("../src/app");
+const User = require("../src/models/user");
+
+const userOne = {
+  name: "Mike",
+  email: "mike@example.com",
+  password: "56what!!",
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
+
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      name: "Andrew",
+      email: "andrew@example.com",
+      password: "MyPass777!",
+    })
+    .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
+
+test("Should not login nonexistent user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: "thisisnotmypass",
+    })
+    .expect(400);
+});
