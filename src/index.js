0de1d20bd70aa6df188692366a0285e9642ca416
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-02-19 07:13:23 +0600

    setup for api test

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
+const app = require("./app");
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
