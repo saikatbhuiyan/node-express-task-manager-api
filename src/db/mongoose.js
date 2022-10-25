e240286201b24e8b8c78108c614dc87c5157847f
saikatbhuiyan <shaikotbhuiya@gmail.com>
2022-10-25 21:05:12 +0600

    bug fixed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,5 +3,5 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   // useCreateIndex: true,
-  useFindAndModify: true,
+  // useFindAndModify: true,
 });
